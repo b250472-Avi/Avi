06785474710698c359d0030cb4e085e3276feef5
AVI SHARMA <b250472@skit.ac.in>
2026-07-11 14:20:11 +0530

    Add files via upload

---
+++ 25ESKCA040/day4/js/arithmetic.js
@@ -0,0 +1,31 @@
+function performArithmeticOperation(){
+        let var1 = 10;
+        const pi = 3.14;
+        const number1 = parseFloat(document.getElementById("number1").value);
+        const number2 = parseFloat(document.getElementById("number2").value);
+        const operator=document.getElementById("operator").value;
+        let result=0;
+    
+        switch(operator){
+            case "+":
+            result = number1+number2;
+            break;
+            case "-":
+            result = number1-number2;
+            break;
+            case "*":
+            result = number1*number2;
+            break;
+            case "/":
+            if(number2==0){
+                result="Not defined";
+            }   
+            else{
+                result=number1/number2;
+            }
+            break;
+
+        }
+        document.getElementById("result").innerText = "result:" + result;
+
+}
